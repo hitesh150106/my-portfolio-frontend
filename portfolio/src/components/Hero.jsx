6948fc955dcd6f261dd84480bb6a112308397538
Hitesh Bidawatka <bidawatkahitesh4@gmail.com>
2026-01-26 20:40:51 +0530

    updated code-2

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
     <section className="relative w-full h-screen mx-auto">
       <img src={herobg} alt="hero-background" className="absolute inset-0 w-full h-full object-cover z-[-1]"/>
 
-      <div className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5 left-[38%] -translate-x-1/2`}>
+      <div className={`absolute inset-0 top-[90px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5 left-[38%] -translate-x-1/2`}>
         
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -25,10 +25,27 @@ const Hero = () => {
             Idea to Real World Porduct.
           </p>
         </div>
-        
       </div>
 
-      <ComputersCanvas />
+      <div className="absolute inset-0 z-0 pointer-events-none">
+        <ComputersCanvas />
+      </div>
+      
+      <div className="absolute xs:bottom-10 bottom-6 w-full flex justify-center items-center z-20">   
+        <a href="#about">
+          <div className="w-[34px] h-[64px] rounded-3xl border-4 border-bg-gray-300 flex justify-center items-start p-3">        
+            <motion.div 
+            animate={{ y : [0, 40, 0]}}
+            transition = {{
+              duration : 1.5,
+              repeat : Infinity,
+              repeatType : 'loop'
+            }}
+            className="w-3 h-3 rounded-full bg-gray-300 mb-1"
+            />
+          </div>
+        </a> 
+      </div>
     </section>
   );
 };
--- portfolio/src/components/Loader.jsx
+++ portfolio/src/components/Loader.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+// import { useProgress , Html } from '@react-three/drei';
 
-const Loader = () => {
+// const Loader = () => {
+//   const { progress } = useProgress
+//   return (
+//     <Html>
+//       <span className="canvas-load"></span>
+//       <p
+//       style={{
+//         fontSize : 14,
+//         color : '#f1f11f1',
+//         fontWeight : 800,
+//         margintop : 40
+//       }
+//       }>
+//         {progress.toFixed(2)}%
+//       </p>
+//     </Html>
+//   )
+// }
+
+// export default Loader
+
+import { useProgress } from '@react-three/drei';
+
+const CanvasLoader = () => {
+  const { progress } = useProgress();
+
   return (
-    <div></div>
-  )
-}
+    <div className="w-[75px] h-[75px] rounded-full border-4 border-[#915eff] border-t-transparent animate-spin flex justify-center items-center">
+      <span className="absolute text-[#915eff] font-bold text-sm">
+        {progress.toFixed(0)}%
+      </span>
+    </div>
+  );
+};
 
-export default Loader
+export default CanvasLoader;
