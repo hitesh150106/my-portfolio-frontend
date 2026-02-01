dc9dcde6e494f257f421b1ad85b1d6c1424933ac
Hitesh Bidawatka <bidawatkahitesh4@gmail.com>
2026-02-01 17:07:28 +0530

    Add prop-types dependency

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,12 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:5000"
-    }
-  },
-    optimizeDeps: {
-    include: ['react-vertical-timeline-component'],
-  },
+  base: "/",
 });
